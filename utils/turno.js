1f3216f80bb9b354e6a57096fad9c2379d8592ff
LS <luissolorzano88@gmail.com>
2026-06-09 19:46:32 -0400

    Create turno.js

---
+++ utils/turno.js
@@ -0,0 +1,8 @@
+import { timeToMin } from './time';
+
+export function getTurnoByCodigo(sheetName) { ... }
+export function normalizeTurno(t) { ... }
+export function getTurnoFromHora(horaStr) { ... }
+export function getTurnoDeRegistro(d) { ... }
+export function getBloquesForTurno(turno) { ... }
+export function findStartBlock(bloques, horaStr) { ... }
