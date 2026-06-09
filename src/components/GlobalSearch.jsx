c330ad7a8e233b5f78c02bd28cbcfce5adf41fed
LS <luissolorzano88@gmail.com>
2026-06-09 19:56:35 -0400

    Create GlobalSearch.jsx

---
+++ src/components/GlobalSearch.jsx
@@ -0,0 +1,4 @@
+import { useState, useMemo, useEffect, useRef } from 'react';
+import { TRAYECTO_COLORS, TRAYECTO_BG } from '../constants';
+import { parseClase } from '../utils/parsing';
+// estilos inline (S) se pueden mantener aquí o mover a un objeto compartido
