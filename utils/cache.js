d8c30074cc116643bceaf2254fd841a5f72b0508
LS <luissolorzano88@gmail.com>
2026-06-09 19:47:27 -0400

    Create cache.js

---
+++ utils/cache.js
@@ -0,0 +1,7 @@
+export const CACHE_KEYS = {...};
+export const CACHE_EXPIRY = ...;
+
+export function guardarEnCache(key, datos) { ... }
+export function cargarDeCache(key) { ... }
+export function limpiarCache() { ... }
+export function obtenerUltimaSincronizacion() { ... }
